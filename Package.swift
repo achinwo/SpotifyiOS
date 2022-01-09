b0c2651c446c0e1b911fcb13bfc59e12481b94b9
Anthony Chinwo <anthony.chinwo@gmail.com>
2022-01-09 22:32:10 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,32 +19,7 @@ let package = Package(
     targets: [
         .binaryTarget(
               name: "SpotifyiOS",
-              url: "// swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
-import PackageDescription
-
-let package = Package(
-    name: "SpotifyiOS",
-    platforms: [
-        .iOS(.v13)
-    ],
-    products: [
-        .library(
-            name: "SpotifyiOS",
-            targets: ["SpotifyiOS"])
-    ],
-    dependencies: [
-        // No dependencies!
-    ],
-    targets: [
-        .binaryTarget(
-              name: "SpotifyiOS",
               url: "https://github.com/achinwo/SpotifyiOS/releases/download/v1.3.0/SpotifyiOS.xcframework.zip",
-              checksum: "061fdb53f602f0da367931b04fd1d199e3639cb040e72bb10308ae3e8d96f4d1"
-        )
-    ]
-)",
               checksum: "061fdb53f602f0da367931b04fd1d199e3639cb040e72bb10308ae3e8d96f4d1"
         )
     ]
